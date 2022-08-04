9fb1c4e1405b02f77df9c827a7f31dc37501f914
Angela Salomão <angelasalomaokl@gmail.com>
2022-08-04 15:33:54 -0300

    ajuste de css global

--- src/components/PlanetCard/PlanetCard.js
+++ src/components/PlanetCard/PlanetCard.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import './PlanetCard.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
 
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="card-planet">
         <p data-testid="planet-name">{ planetName }</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
